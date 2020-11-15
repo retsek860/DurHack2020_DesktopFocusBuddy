99c230053a9ed0033890ad2d892743192d134724
Nsilz44 <n_stbss@hotmail.com>
2020-11-15 11:02:21 +0000

    added steppe.exe to website

--- website/client/index.js
+++ website/client/index.js
@@ -1,4 +1,10 @@
-document.getElementById('button').addEventListener('click', getExe());
+document.getElementById('pepehands').onclick = getExe();
+//h.addEventListener('click', getExe());
 function getExe() {
-    return executable
+    let a = document.createElement('a')
+    a.href = 'Steppe.exe'
+    a.download = a.href.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
 }
